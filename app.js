f06d5b5c91ae321fdbdbe48dfa59eabd93b04426
badat <you@example.com>
2020-03-03 16:21:05 +0700

    Funny circles

---
+++ app.js
@@ -0,0 +1,29 @@
+function Circle(x, y, radius){
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+
+function getRandomHex(){
+    return Math.floor(Math.random()*255);
+}
+
+function getRandomColor(){
+    let red = getRandomHex();
+    let green = getRandomHex();
+    let blue = getRandomHex();
+    return "rgb(" + red + "," + blue + "," + green +")";
+}
+
+function createCircle(){
+    let ctx = document.getElementById("MyCanvas").getContext("2d");
+    let radius = Math.floor(Math.random() * 200);
+    let color = getRandomColor();
+    let circle= new Circle(500, 500, radius);
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+createCircle();
